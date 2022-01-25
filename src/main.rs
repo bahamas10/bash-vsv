1583d791141d33012a3278de5a9fa6cf954a01be
Dave Eddy <dave@daveeddy.com>
2022-01-25 15:03:07 -0500

    refactor into structs and enums (still does nothing)

--- src/main.rs
+++ src/main.rs
@@ -8,64 +8,129 @@
  * License: MIT
  */
 
+use std::env;
+use std::fmt;
 use std::fs;
 use std::path;
+use std::ffi::OsString;
 
 use anyhow::{anyhow, Result};
 
-#[derive(Debug)]
+const DEFAULT_DIR: &str = "/var/service";
+
 enum ServiceState {
     Run,
     Down,
-    Finish
+    Finish,
+    Unknown
 }
 
-static BASE_DIR: &str = "./test/service";
-
-fn main() {
-    let services = match get_services() {
-        Ok(svcs) => svcs,
-        Err(err) => panic!("failed to get services: {:?}", err),
-    };
-
-    println!("services = {:?}", services);
-
-    for service in services {
-        let _ = process_service(service);
+impl fmt::Display for ServiceState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = match self {
+            ServiceState::Run => "run",
+            ServiceState::Down => "down",
+            ServiceState::Finish => "finish",
+            ServiceState::Unknown => "---",
+        };
+        s.fmt(f)
     }
 }
 
-fn process_service(dir: path::PathBuf) -> Result<()> {
-    let name = match dir.file_name() {
+impl ServiceState {
+    fn get_char(&self) -> String {
+        let s = match self {
+            ServiceState::Run => "✔",
+            ServiceState::Down => "X",
+            ServiceState::Finish => "X",
+            ServiceState::Unknown => "?",
+        };
+
+        String::from(s)
+    }
+}
+
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct Service {
+    path: path::PathBuf,
+}
+
+impl Service {
+    pub fn new(path: path::PathBuf) -> Self {
+        Self {
+            path
+        }
+    }
+
+    pub fn wants_down(&self) -> bool {
+        let path = self.path.join("down");
+
+        path.exists()
+    }
+
+    pub fn get_pid(&self) -> Option<u32> {
+        let path = self.path.join("supervise").join("pid");
+
+        if let Ok(data) = fs::read_to_string(path) {
+            if let Ok(pid) = data.trim().parse::<u32>() {
+                return Some(pid);
+            }
+        }
+
+        None
+    }
+
+    pub fn get_state(&self) -> ServiceState {
+        let path = self.path.join("supervise").join("stat");
+
+        if let Ok(s) = fs::read_to_string(path) {
+            return match s.trim() {
+                "run" => ServiceState::Run,
+                "down" => ServiceState::Down,
+                "finish" => ServiceState::Finish,
+                _ => ServiceState::Unknown,
+            };
+        }
+
+        ServiceState::Unknown
+    }
+}
+
+fn process_service(service: &Service) -> Result<()> {
+    // extract service name from path (basename)
+    let name = match service.path.file_name() {
         Some(name) => name,
         None => return Err(anyhow!("failed to get name from service")),
     };
-
-    println!("dir = {:?}", dir);
-    println!("name = {:?}", name);
-
-    let pid = match get_pid(&dir) {
-        Ok(pid) => pid,
-        Err(err) => panic!("failed to get pid: {:?}", err),
+    let name = match name.to_str() {
+        Some(name) => name,
+        None => return Err(anyhow!("failed to parse name from service")),
     };
 
-    println!("pid = {:?}", pid);
+    let wants_down = service.wants_down();
+    let pid = service.get_pid();
+    let state = service.get_state();
 
-    let state = match get_state(&dir) {
-        Ok(state) => state,
-        Err(err) => panic!("failed to get state: {:?}", err),
+    let down = match wants_down {
+        true => "down",
+        false => "---"
+    };
+    let pid_s = match pid {
+        Some(pid) => pid.to_string(),
+        None => String::from("---")
     };
 
-    println!("state = {:?}", state);
+    println!("  {:1} {:10} {:10} {:10} {:10}",
+        state.get_char(), name, state, pid_s, down);
 
     Ok(())
 }
 
-fn get_services() -> Result<Vec<path::PathBuf>> {
+fn get_services() -> Result<Vec<Service>> {
     // loop services directory and collect service names
     let mut dirs = Vec::new();
 
-    for entry in fs::read_dir(BASE_DIR)? {
+    for entry in fs::read_dir(".")? {
         let entry = entry?;
         let path = entry.path();
 
@@ -73,7 +138,9 @@ fn get_services() -> Result<Vec<path::PathBuf>> {
             continue;
         }
 
-        dirs.push(path);
+        let service = Service::new(path);
+
+        dirs.push(service);
     }
 
     dirs.sort();
@@ -81,21 +148,26 @@ fn get_services() -> Result<Vec<path::PathBuf>> {
     Ok(dirs)
 }
 
-fn get_pid(buf: &path::Path) -> Result<u32> {
-    let path = buf.join("supervise").join("pid");
-    let data: u32 = fs::read_to_string(path)?.trim().parse()?;
+fn main() {
+    // cd into SVDIR or the default dir
+    let svdir = match env::var_os("SVDIR") {
+        Some(dir) => dir,
+        None => OsString::from(DEFAULT_DIR),
+    };
+    let svdir = path::Path::new(&svdir);
+    env::set_current_dir(&svdir).expect("failed to cd to SVDIR");
 
-    Ok(data)
-}
+    // find all services
+    let services = match get_services() {
+        Ok(svcs) => svcs,
+        Err(err) => panic!("failed to get services: {:?}", err),
+    };
 
-fn get_state(buf: &path::Path) -> Result<ServiceState> {
-    let path = buf.join("supervise").join("stat");
-    let s = fs::read_to_string(path)?;
+    println!("  {:1} {:10} {:10} {:10} {:10}",
+        "", "NAME", "STATE", "PID", "DOWN");
 
-    match s.trim() {
-        "run" => Ok(ServiceState::Run),
-        "down" => Ok(ServiceState::Down),
-        "finish" => Ok(ServiceState::Finish),
-        _ => Err(anyhow!("unknown service state: '{:?}'", s)),
+    // process each service found
+    for service in services {
+        let _ = process_service(&service);
     }
 }
