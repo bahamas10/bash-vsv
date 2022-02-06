3c2fbefabd6f45e0cdae4ffa195a1f27ddba2fe4
Dave Eddy <dave@daveeddy.com>
2022-02-06 14:46:49 -0500

    option names and text

--- src/arguments.rs
+++ src/arguments.rs
@@ -5,22 +5,27 @@ use clap::{Parser, Subcommand};
 #[derive(Debug, Parser)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    /// kill yourself homo
-    #[clap(short, long)]
+    /// Enable or disable color output
+    #[clap(short, long, value_name = "yes|no|auto")]
     pub color: Option<String>,
 
-    #[clap(short, long, parse(from_os_str), value_name = "FILE")]
+    /// Directory to look into, defaults to env SVDIR or /var/service if unset
+    #[clap(short, long, parse(from_os_str), value_name = "dir")]
     pub dir: Option<path::PathBuf>,
 
+    /// Show log processes, this is a shortcut for 'status -l'
     #[clap(short, long)]
     pub log: bool,
 
+    /// Tree view, this is a shortcut for 'status -t'
     #[clap(short, long)]
     pub tree: bool,
 
+    /// User mode, this is a shortcut for '-d ~/runit/service'
     #[clap(short, long)]
     pub user: bool,
 
+    /// Increase Verbosity
     #[clap(short, long, parse(from_occurrences))]
     pub verbose: usize,
 
@@ -30,11 +35,11 @@ pub struct Args {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-    /// does testing things
-    Test {
-        /// lists test values
+    /// Show process status
+    Status {
+        /// Tree view
         #[clap(short, long)]
-        list: bool,
+        tree: bool,
     },
 }
 
